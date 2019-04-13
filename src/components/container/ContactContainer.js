895cae47c5dc8473645cdc1ea59c133b4ad75380
ezequielish <ezequielprophetic@gmail.com>
2019-04-13 08:52:07 -0400

    datails change margin, etc

--- src/components/container/ContactContainer.js
+++ src/components/container/ContactContainer.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-
+import mapa from '../../assets/img/icons/map.svg';
+import email from '../../assets/img/icons/social-network/email.svg';
+import whatsapp from '../../assets/img/icons/social-network/whatsapp.svg';
 class ContactContainer extends Component {
     render() {
         return (
             <section className='Contact'>
                 <h1>Contactanos</h1>
-                <div className='Contact__desctiption'>
+                <div className='Contact__description'>
                     <h2>Ponte en contacto y pide tu presupuesto sin ningún compromiso</h2>
                     <h3>Brindamos toda la información que necesitas saber.</h3>
 
-                    <div className='Contact__desctiption-info'>
-                        <p><img /> +34 666378709</p>
-                        <p><img /> bozicontruccionesreformas@gmail.com</p>
-                        <p><img /> Valencia, España.</p>
+                    <div className='description-info'>
+                        <p><img src={whatsapp} width='20px' height='20px' /> +34 666378709</p>
+                        <p><img src={email} width='20px' height='20px' /> bozicontruccionesreformas@gmail.com</p>
+                        <p><img src={mapa} width='20px' height='20px' /><img /> Valencia, España.</p>
                     </div>
                 </div>
                 <div className='Contact__form'>
                     <form>
-                        <input type='text' />
-                        <input type='email' />
-                        <input type='text' />
-                        <textarea></textarea>
-                        <input type='submit' />
+                        <input type='text' placeholder='Nombre (obligatorio)' />
+                        <input type='email' placeholder='Correo electrónico (obligatorio)' />
+                        <input type='text' placeholder='asunto (obligatorio)' />
+                        <textarea placeholder='Mensaje'></textarea>
+                        <button>ENVIAR</button>
                     </form>
                 </div>
             </section>
