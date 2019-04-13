2ec66839b2aefc43ac0cdd3a1db80e9ee881cf1a
ezequielish <ezequielprophetic@gmail.com>
2019-04-13 09:34:10 -0400

    footer add

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import instagram from '../assets/img/icons/social-network/instagram.svg';
+import fb from '../assets/img/icons/social-network/fb.svg';
 const SocialNetwork = props => {
     return (
         <div className='SocialNetwork'>
-            <p>twitter</p>
-            <p>Instagram</p>
+            <img src={instagram} width='20px' height='20px' />
+            <img src={fb} width='20px' height='20px' />
         </div>
     )
 }
